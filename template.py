35e5aa3cab5eb65d39cca6117a801ea9b0d9f063
Cather-Zhang <cather0215@gmail.com>
2023-10-02 14:26:20 -0400

    New change, testing

--- template.py
+++ template.py
@@ -10,7 +10,7 @@ import numpy as np
 config = {
     'batch_size': 64,
     'image_size': (224,224),
-    'epochs': 20,
+    'epochs': 70,
     'optimizer': 'adam'
 }
 ###########################MAGIC ENDS  HERE##########################
@@ -32,7 +32,7 @@ def read_data():
     return train_ds, val_ds, test_ds
 
 def data_processing(ds):
-    ds = ds.shuffle(1000)
+    ds = ds.shuffle(5000)
     data_augmentation = keras.Sequential(
         [
             ###########################MAGIC HAPPENS HERE##########################
@@ -62,21 +62,16 @@ def build_model(input_shape, num_classes):
     x = layers.Conv2D(input_shape=(224,224,3),filters=64,kernel_size=(3,3),padding="same", activation="relu")(x)
     x = layers.Conv2D(filters=32,kernel_size=(3,3),padding="same", activation="relu")(x)
     x = layers.MaxPooling2D(pool_size=(2,2),strides=(2,2))(x)
-
     x = layers.Conv2D(filters=64, kernel_size=(3,3), padding="same", activation="relu")(x)
     x = layers.Conv2D(filters=64, kernel_size=(3,3), padding="same", activation="relu")(x)
     x = layers.MaxPooling2D(pool_size=(2,2),strides=(2,2))(x)
-
     x = layers.Conv2D(filters=128, kernel_size=(3,3), padding="same", activation="relu")(x)
     x = layers.Conv2D(filters=128, kernel_size=(3,3), padding="same", activation="relu")(x)
     x = layers.MaxPooling2D(pool_size=(2,2),strides=(2,2))(x)
-
     x = layers.Conv2D(filters=256, kernel_size=(3,3), padding="same", activation="relu")(x)
     x = layers.MaxPooling2D(pool_size=(2,2),strides=(2,2))(x)
-
     x = layers.Flatten()(x)
-
-    x = layers.Dense(hidden_units, activation='relu')(x)
+    x = layers.Dense(hidden_units, activation='sigmoid')(x)
 
     ###########################MAGIC ENDS HERE##########################
     outputs = layers.Dense(num_classes, activation="softmax", kernel_initializer='he_normal')(x)
@@ -87,7 +82,7 @@ def build_model(input_shape, num_classes):
 
 # Display the misclassified images
 def plot_misclassified_images(images, labels, predictions, num_images=3):
-    figsize = (20, 20)
+    figsize = (30, 30)
     fig = plt.figure(figsize=figsize)
     
     for i in range(num_images):
@@ -95,22 +90,25 @@ def plot_misclassified_images(images, labels, predictions, num_images=3):
         image = images[idx]
         true_label = labels[idx]
         predicted_label = predictions[idx]
-        
+        grayscale_image = np.mean(image, axis=2)
+
         # Display the misclassified image along with true and predicted labels
         plt.subplot(1, num_images, i+1)
         plt.xticks([])
         plt.yticks([])
         plt.grid(False)
-        plt.imshow(image.astype(np.uint8))
+        plt.imshow(grayscale_image, cmap='gray')
         plt.xlabel(f'True: {true_label}, Predicted: {predicted_label}')
     
     plt.show()
+
 
 if __name__ == '__main__':
     # Load and Process the dataset
     train_ds, val_ds, test_ds = read_data()
     train_ds = data_processing(train_ds)
     # Build up the ANN model
+    
     model = build_model(config['image_size']+(3,), 5)
     # Compile the model with optimizer and loss function
     model.compile(
@@ -131,13 +129,20 @@ if __name__ == '__main__':
     test_images = np.concatenate([x for x, y in test_ds], axis=0)
     test_labels = np.concatenate([y for x, y in test_ds], axis=0)
     test_prediction = np.argmax(model.predict(test_images),1)
+    
+    #model = keras.models.load_model("77.8.h5")
+    model.save("new_sigmoid.h5")
 
-    model.save('Model_12.h5')
-
+    test_loss, test_acc = model.evaluate(test_ds, verbose=2)
+    print("\nTest Accuracy: ", test_acc)
+    test_images = np.concatenate([x for x, y in test_ds], axis=0)
+    test_labels = np.concatenate([y for x, y in test_ds], axis=0)
+    test_prediction = np.argmax(model.predict(test_images),1)
+    
     # 1. Visualize the confusion matrix by matplotlib and sklearn based on test_prediction and test_labels
     ConfusionMatrixDisplay.from_predictions(test_labels, test_prediction)
     plt.show()
-    
+
     # 2. Report the precision and recall for 10 different classes
     # Hint: check the precision and recall functions from sklearn package or you can implement these function by yourselves.
     print(classification_report(test_labels, test_prediction))
@@ -151,8 +156,3 @@ if __name__ == '__main__':
 
     # Display three random misclassified images
     plot_misclassified_images(test_images, test_labels, test_prediction, num_images=3)
-
-
-
-    ###########################MAGIC HAPPENS HERE##########################
-    
